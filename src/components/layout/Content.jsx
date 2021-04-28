a82d606cec61b88a24d8aec0870fc22e1880a560
everibas <ribas.analista@gmail.com>
2021-04-28 01:00:34 +0100

    Navegação com React Router

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,9 +1,27 @@
 import './Content.css'
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import About from '../../views/exemples/About';
+import Home from '../../views/exemples/Home';
+import Param from '../../views/exemples/Param';
+import NotFound from '../../views/exemples/NotFound';
 
 const Content = props => (
   <main className="Content">
-    Componente Content
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/param/:id">
+        <Param />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
   </main>
 )
 
